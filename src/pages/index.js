a856bd7016aef201b5964ae3e24a6085bad38387
Ellie Wang <yuetingw@outlook.com>
2022-02-16 18:18:58 +0800

    updated main contents

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import {
-  AboutSection,
-  ArticlesSection,
+  // AboutSection,
+  // ArticlesSection,
   ContactSection,
   HeroSection,
   InterestsSection,
@@ -13,14 +13,14 @@ import {
 export default function IndexPage() {
   return (
     <>
-      <Seo title="Gatsby Starter for Portfolio Minimal" />
+      <Seo title="Ellie W Portfolio" />
       <Page useSplashScreenAnimation>
-        <HeroSection sectionId="hero" />
-        <ArticlesSection sectionId="articles" heading="Latest Articles" sources={['Medium']} />
-        <AboutSection sectionId="about" heading="About Portfolio Minimal" />
-        <InterestsSection sectionId="details" heading="Details" />
-        <ProjectsSection sectionId="features" heading="Built-in Features" />
-        <ContactSection sectionId="github" heading="Issues?" />
+        <HeroSection sectionId="about" />
+        {/* <ArticlesSection sectionId="articles" heading="Latest Articles" sources={['Medium']} /> */}
+        {/* <AboutSection sectionId="about" heading="About Portfolio Minimal" /> */}
+        <InterestsSection sectionId="stacks" heading="Stacks" />
+        <ProjectsSection sectionId="projects" heading="Projects" />
+        <ContactSection sectionId="contacts" heading="Contact" />
       </Page>
     </>
   );
